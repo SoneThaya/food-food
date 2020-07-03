61f1e8e3adf2e6a69469e09ba354df8cb0dbcd6e
Sone <sthaya@gmail.com>
2020-07-03 13:19:36 -0700

    created axios call got data from api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Menu from './pages/Menu'
+import {axiosWithAuth} from './utils/axiosWithAuth'
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([])
+
+  const getList = () => {
+    axiosWithAuth()
+      .get('https://grub-grub-backend.herokuapp.com/api/menu')
+      .then(res => {
+        console.log(res.data)
+        setList(res.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getList()
+  }, [])
+
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/menu' exact component={Menu} list={list} />
         </Switch>
       </Router>
     </div>
