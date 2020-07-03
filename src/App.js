485f048a2733bb49d5ef9eea2cac0c81e00ffcda
Sone <sthaya@gmail.com>
2020-07-03 13:42:20 -0700

    added material ui cards to display data

--- src/App.js
+++ src/App.js
@@ -3,25 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Menu from './pages/Menu'
-import {axiosWithAuth} from './utils/axiosWithAuth'
+
 import './App.css';
 
 function App() {
-  const [list, setList] = useState([])
-
-  const getList = () => {
-    axiosWithAuth()
-      .get('https://grub-grub-backend.herokuapp.com/api/menu')
-      .then(res => {
-        console.log(res.data)
-        setList(res.data)
-      })
-      .catch(err => console.log(err))
-  }
-
-  useEffect(() => {
-    getList()
-  }, [])
+  
 
   return (
     <div className="App">
@@ -29,7 +15,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/menu' exact component={Menu} list={list} />
+          <Route path='/menu' exact component={Menu} />
         </Switch>
       </Router>
     </div>
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Grid from '@material-ui/core/Grid'
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const Menu = (props) => {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+
+function Menu() {
+ 
+  const classes = useStyles();
+
+  const [list, setList] = useState([])
+
+  const getList = () => {
+    axiosWithAuth()
+      .get('https://grub-grub-backend.herokuapp.com/api/menu')
+      .then(res => {
+        console.log(res.data)
+        setList(res.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getList()
+  }, [])
+
   return (
-    <div>
-      hello
-    </div>
-  )
+    <Grid container spacing={3}>
+      {list.map((item => (
+        <Grid item sx={12} sm={6} md={4} key={item.id}>
+          <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              alt={item.title}
+              height="140"
+              image={item.itemImage}
+              title={item.title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {item.title}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {item.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+    </Grid>
+      )))}
+    </Grid>
+  );
 }
 
 export default Menu
